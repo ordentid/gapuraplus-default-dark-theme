dcc28b9fcb8605e6dd2dba4c964a0e08178a94a1
Rachmat Priambudi <rachmat.keybladers93@gmail.com>
2019-10-03 14:00:07 +0700

    - implement contact page - remove deprecated code

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -38,26 +38,18 @@
       >
         <v-layout row wrap justify-center align-center style="height: 100%;">
           <v-toolbar-side-icon @click="drawer = !drawer" class="hover hidden-sm-and-up" style="color: whitesmoke;"/>
-          <template v-if="config.use_home">
-            <template v-if="homeSection.use_icon">
-              <v-btn
-                icon
-                @click.stop="miniVariant = !miniVariant"
-              >
-                <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
-              </v-btn>
-            </template>
-            <template v-else>
-              <v-toolbar-title class="title default-color text-xs-center mr-5" @click.prevent="$ksvuefp.$emit('ksvuefp-nav-click', {nextIndex: homeSection.section_id})">{{ homeSection.section_menu }}</v-toolbar-title>
-            </template>
-          </template>
-          <template
-            v-for="toolbarSection in menuSections"
-          >
-            <v-btn flat :key="toolbarSection.id" depressed color="white" @click.prevent="$ksvuefp.$emit('ksvuefp-nav-click', {nextIndex: toolbarSection.section_id})" class="hidden-sm-and-down body-2 font-weight-regular">
-              {{ toolbarSection.section_menu }}
-            </v-btn>
-          </template>
+          <v-btn flat depressed color="white" @click.prevent="$ksvuefp.$emit('ksvuefp-nav-click', {nextIndex: 0})" class="hidden-sm-and-down body-2 font-weight-regular">
+            Beranda
+          </v-btn>
+          <v-btn flat depressed color="white" @click.prevent="$ksvuefp.$emit('ksvuefp-nav-click', {nextIndex: 1})" class="hidden-sm-and-down body-2 font-weight-regular">
+            Profil
+          </v-btn>
+          <v-btn flat depressed color="white" @click.prevent="$ksvuefp.$emit('ksvuefp-nav-click', {nextIndex: 2})" class="hidden-sm-and-down body-2 font-weight-regular">
+            Layanan
+          </v-btn>
+          <v-btn flat depressed color="white" @click.prevent="$ksvuefp.$emit('ksvuefp-nav-click', {nextIndex: 3})" class="hidden-sm-and-down body-2 font-weight-regular">
+            Kontak
+          </v-btn>
         </v-layout>
       </v-toolbar>
       <v-toolbar
@@ -71,17 +63,6 @@
       >
         <v-layout xs12 justify-left align-center>
           <v-toolbar-side-icon @click="drawer = !drawer" class="hover hidden-sm-and-up toolbar"/>
-          <template v-if="config.use_icons">
-            <v-btn
-              icon
-              @click.stop="miniVariant = !miniVariant"
-            >
-              <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
-            </v-btn>
-          </template>
-          <template v-else>
-            <v-toolbar-title class="title default-color" @click.prevent="$ksvuefp.$emit('ksvuefp-nav-click', {nextIndex: homeSection.section_id})">{{ homeSection.section_menu }}</v-toolbar-title>
-          </template>
         </v-layout>
       </v-toolbar>
       <ksvuefp-section
@@ -172,7 +153,7 @@
                   </v-layout>
               </v-parallax>
             </v-layout>
-            <v-carousel hide-delimiters dark style="height: 55%; width: 100%;" @click.native="carouselClick">
+            <v-carousel hide-delimiters dark style="height: 55%; width: 100%;">
               <v-carousel-item
                 v-for="(carousel, index) in productCarousel"
                 :key="index"
@@ -210,7 +191,7 @@
                   </v-layout>
               </v-parallax>
             </v-layout>
-            <v-carousel hide-delimiters dark style="height: 50%; width: 100%;" @click.native="carouselClick">
+            <v-carousel hide-delimiters dark style="height: 50%; width: 100%;">
               <v-carousel-item
                 v-for="(carousel, index) in productCarousel"
                 :key="index"
@@ -236,6 +217,57 @@
               </v-carousel-item>
             </v-carousel>
           </v-layout>
+      </ksvuefp-section>
+      <ksvuefp-section
+        class="content-wrapper"
+        :section="socialMedia"
+        :key="'contact'"
+        :section-index=3
+      >
+        <v-layout column fill-width ma-0 pa-0 hidden-sm-and-down odd-content section-content>
+          <v-toolbar color="#1C1B20" flat fill-width depressed style="color:whitesmoke; position: relative; height: 5%;">
+            <v-layout row wrap fill-width fill-height justify-center align-center>
+              <v-toolbar-title class="title strong text-xs-center mr-5 pr-5 align-center" v-text="'Send email to us for further inquiry'" />
+              <v-toolbar-items class="ml-5 pl-5">
+                <v-btn :href="'test@mail.co'" style="height: 80%">Contact Us</v-btn>
+              </v-toolbar-items>
+            </v-layout>
+          </v-toolbar>
+          <v-layout row wrap fill-width align-center justify-center> 
+            <template v-for="(contact, i) in socialMedia">
+              <v-avatar size="44px" :key="i" color="grey" class="mx-3 clickable" @click="goToLink(contact.url)">
+                <v-img :src="contact.icon_src" max-height="30" max-width="30"></v-img>
+              </v-avatar>
+            </template>
+          </v-layout>
+          <v-toolbar color="#1C1B20" flat fill-width depressed style="color:whitesmoke; height: 5%;">
+            <v-layout row wrap fill-width justify-center>
+              <v-toolbar-title class="body-2" v-text="'Created by Ordent'" />
+            </v-layout>
+          </v-toolbar>
+        </v-layout>
+        <v-layout column fill-height fill-width ma-0 hidden-sm-and-up pa-0 even-content section-content-mobile>
+          <v-toolbar color="#1C1B20" flat depressed style="color:whitesmoke;height: 15%;" class="py-1">
+            <v-layout column wrap fill-width justify-center>
+              <v-toolbar-title class="subheading strong text-xs-center pb-2" v-text="'Send email to us for further inquiry'" />
+              <v-toolbar-items class="justify-center pb-3">
+                <v-btn :href="'test@mail.co'">Contact Us</v-btn>
+              </v-toolbar-items>
+            </v-layout>
+          </v-toolbar>
+          <v-layout row wrap fill-width align-center justify-center>
+            <template v-for="(contact, i) in socialMedia">
+              <v-avatar size="44px" :key="i" color="grey" class="mx-3 clickable" @click="goToLink(contact.url)">
+                <v-img :src="contact.icon_src" max-height="30" max-width="30"></v-img>
+              </v-avatar>
+            </template>
+          </v-layout>
+          <v-toolbar color="#1C1B20" flat fill-width depressed style="color:whitesmoke; height: 5%;">
+            <v-layout xs12 row wrap fill-width justify-center>
+              <v-toolbar-title class="caption" v-text="'Created by Ordent'" />
+            </v-layout>
+          </v-toolbar>
+        </v-layout>
       </ksvuefp-section>
     </ksvuefp>
   </v-layout>
@@ -283,6 +315,15 @@
           description
         }
       }
+    },
+    socialMedia: allSocialMedia (filter: {id: {ne: "dummy"}}) {
+      edges {
+        node {
+          id,
+          type,
+          url
+        }
+      }
     }
   }
 </page-query>
@@ -302,12 +343,6 @@ export default {
   computed: {
     currentIndex() {
       return this.$ksvuefp.currentIndex
-    },
-    meta() {
-      return this.$store.state.meta
-    },
-    config() {
-      return this.$store.state.config
     },
     welcomePost() {
       let welcomePost = {
@@ -398,35 +433,39 @@ export default {
           }
         }
       }
-      console.log(productCarousel)
       return productCarousel
     },
-    contactList() {
-      let socialMedia = Object.assign([], this.$store.state.contactList)
+    socialMedia() {
+      let socialMediaModel = this.$page.socialMedia.edges
+      let socialMediaList = []
 
-      socialMedia.forEach(element => {
-        switch(element.value.name) {
+      let currentObject = null
+      for (let i = 0; i < socialMediaModel.length; i++){
+        currentObject = socialMediaModel[i].node
+        switch(currentObject.type) {
           case 'facebook':
-            element.value.icon_src = './images/facebook-icon.png'
+            currentObject.icon_src = './images/facebook-icon.png'
             break
           case 'instagram':
-            element.value.icon_src = './images/instagram-icon.png'
+            currentObject.icon_src = './images/instagram-icon.png'
             break
           case 'twitter':
-            element.value.icon_src = './images/twitter-icon.png'
+            currentObject.icon_src = './images/twitter-icon.png'
             break
           case 'linkedin':
-            element.value.icon_src = './images/linkedin-icon.png'
+            currentObject.icon_src = './images/linkedin-icon.png'
             break
           case 'whatsapp':
-            element.value.icon_src = './images/whatsapp-icon.png'
-            element.value.value = 'https://wa.me/62' + element.value.value
+            currentObject.icon_src = './images/whatsapp-icon.png'
+            currentObject.url = 'https://wa.me/62' + currentObject.url
             break
           default:
             console.log('No Data')
         }
-      })
-      return socialMedia
+        socialMediaList.push(currentObject)
+      }
+
+      return socialMediaList
     }
   },
   data() {
@@ -442,9 +481,6 @@ export default {
         dotNavEnabled: false,
         parallax: true
       },
-      headers: {
-        'Project-ID': process.env.GRIDSOME_PROJECT_ID
-      },
       clipped: false,
       drawer: false,
       miniVariant: false,
@@ -457,7 +493,7 @@ export default {
       limit: 2
     }
   },
-  async mounted() {
+  mounted() {
     let Velocity = require('velocity-animate')
     let hammerjs = require('hammerjs')
 
@@ -468,301 +504,11 @@ export default {
     goToLink(urlLink) {
       window.open(urlLink, '_blank')
     },
-    async loadSections(headers, section = null) {
-      let sections = []
-      let contentSections = []
-      await this.$store.dispatch('fetchConfig', headers)
-
-      let sectionId = 0
-      let config = this.config
-      if (headers.Preview){
-        if (section == 2){
-          this.homeSection = {
-            id: 1,
-            section_name: 'home',
-            section_menu: config.home.menu_name,
-            section_id: sectionId,
-            use_icon: config.home.use_icon,
-            icon_url: config.home.menu_icon
-          }
-          contentSections.push(this.homeSection)
-
-          this.config = {
-            title: 'User Config',
-            id: 1,
-            use_products: false,
-            use_contacts: false,
-            use_home: true,
-            use_home_icon: false,
-            home_menu: config.home.menu_name,
-            use_profiles: false,
-          }
-        } else if (section == 3) {
-          sections.push({
-            id: 1, 
-            sectionName: 'profile',
-            sectionMenu: config.profile.menu_name,
-            sectionId: sectionId
-          })
-          await this.fetchProfilePost(headers)
-
-          this.config = {
-            title: 'User Config',
-            id: 1,
-            use_products: false,
-            use_contacts: false,
-            use_home: false,
-            use_home_icon: false,
-            use_profiles: false,
-            profile_menu: config.profile.menu_name
-          }
-        } else if(section == 4) {
-          sections.push({
-            id: 1, 
-            sectionName: 'contacts',
-            sectionMenu: config.contact.menu_name,
-            sectionId: sectionId
-          })
-          await this.fetchProductPost(headers)
-          await this.fetchProductList(this.page, this.limit, headers)
-
-          this.config = {
-            title: 'User Config',
-            id: 1,
-            use_products: true,
-            product_menu: config.contact.menu_name,
-            use_contacts: false,
-            use_home: false,
-            use_home_icon: false,
-            use_profiles: false
-          }
-        }
-      } else {
-        if (config.home){
-          let home = config.home
-
-          this.homeSection = {
-            id: 1,
-            section_name: 'home',
-            section_menu: home.menu_name,
-            section_id: sectionId,
-            use_icon: home.use_icon,
-            icon_url: home.menu_icon
-          }
-
-          contentSections.push(this.homeSection)
-          sectionId++
-        }
-        if (config.profile){
-            sections.push({
-            id: 2, 
-            section_name: 'profile',
-            section_menu: config.profile.menu_name,
-            section_id: sectionId
-          })
-          sectionId++
-        }
-        if (config.product){
-          sections.push({
-            id: 3,
-            section_name: 'product',
-            section_menu: config.product.menu_name,
-            section_id: sectionId,
-          })
-          sectionId++
-        }
-        if (config.contact){
-          sections.push({
-            id: 4,
-            section_name: 'contact',
-            section_menu: config.contact.menu_name,
-            section_id: sectionId,
-          })
-          sectionId++
-        }
-      }
-
-      this.menuSections = sections
-      this.contentSections = contentSections.concat.apply(contentSections, sections)
-    },
-    async fetchContactList(headers) {
-      try {
-        await this.$store.dispatch('fetchContactList', headers)
-      }catch(error) {
-        console.log(error)
-      }
-      this.pageLoading = false
-    },
-    async fetchProductPost(headers) {
-      try {
-        await this.$store.dispatch('fetchProductPost', headers)
-      }catch(error) {
-        console.log(error)
-      }
-      this.pageLoading = false
-    },
-    async fetchProductList(page, limit, headers) {
-      this.carouselLoading = true
-      try {
-        this.page = page
-        this.limit = limit
-
-        await this.$store.dispatch('fetchProductList', {
-          page: this.page,
-          limit: this.limit,
-          headers: headers
-        })
-      }catch(error) {
-        console.log(error)
-      }
-      this.carouselLoading = false
-    },
-    async fetchProfilePost(headers) {
-      try {
-        await this.$store.dispatch('fetchProfilePost', headers)
-      } catch(error) {
-        console.log(error)
-      }
-      this.pageLoading = false
-    },
-    async carouselClick(param) {
-      if (!this.carouselLoading){
-        let halfScreen = Math.floor(screen.width / 2)
-        if (param.clientX > halfScreen){
-          if (this.page < this.meta.lastPage){
-            this.page++
-            await this.fetchProductList(this.page, this.limit, this.headers)
-          }
-        } else {
-          if (this.page > 1){
-            this.page--
-            await this.fetchProductList(this.page, this.limit, this.headers)
-          }
-        }
-      }
-    }
   },
-  watch: {
-    // currentIndex: async function(newIndex, oldIndex) {
-    //   if (newIndex == 0){
-    //     this.pageLoading = true
-    //   } else if (newIndex == 1) {
-    //     this.pageLoading = true
-    //     await this.fetchProfilePost(this.headers)
-    //   } else if (newIndex == 2) {
-    //     this.pageLoading = true
-    //     await this.fetchProductPost(this.headers)
-    //     await this.fetchProductList(1, 2, this.headers)
-    //   } else if (newIndex == 3) {
-    //     this.pageLoading = true
-    //     await this.fetchContactList(this.headers)
-    //   }
-    // }
-  }
 }
 </script>
 
 <style>
-/* body {
-  margin: 0;
-  height: 100%;
-}
-html {
-  height: 100%;
-  font-family: 'Roboto', sans-serif;
-}
-.ksvuefp-section {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.fade-enter-active, .fade-leave-active {
-  transition: all .5s;
-  opacity: 1;
-}
-.fade-enter, .fade-leave-active {
-  opacity: 0;
-}
-.ksvuefp-section, .ksvuefp-section__overlay {
-  height: 0%;
-  width: 0%;
-  top: 0;
-  left: 0;
-}
-.ksvuefp-section, .ksvuefp-section__content {
-  height: 100%;
-  width: 100%;
-  top: 0;
-  left: 0;
-  position: relative;
-}
-.ksvuefp-section, .ksvuefp-section__content, .content-wrapper {
-  position: relative;
-}
-
-.ksvuefp-sections {
-  height: 100%;
-}
-.default-color {
-  color: whitesmoke
-}
-.v-toolbar__content {
-  height: 100% !important;
-  padding: 0 0;
-}
-#app {
-  overflow: hidden;
-}
-.section-content {
-  height: 85%; 
-  max-width: 100%; 
-  width: 100%; 
-  position: absolute; 
-  bottom: 0;
-}
-.section-content-mobile {
-  height: 90%; 
-  max-width: 100%; 
-  width: 100%; 
-  position: absolute; 
-  bottom: 0;
-}
-.default-color {
-  color: whitesmoke
-}
-.even-content {
-  background-color: #24232A;
-  color: whitesmoke;
-}
-.odd-content {
-  background-color: #1C1B20;
-  color: whitesmoke;
-}
-.cover-image-full {
-  height: 100%;
-  width: 100%;
-}
-.cover-image-not-full {
-  height: 100%;
-  width: 30%;
-  background-color: #1C1B20;
-}
-.cover-image-not-full-mobile {
-  height: 40%;
-  width: 100%;
-  background-color: #1C1B20;
-}
-.product-header {
-  height: 35%;
-  width: 100%
-}
-.product-detail {
-  height: 100%;
-  width: 100%
-}
-.clickable {
-  cursor: pointer;
-} */
 body {
   margin: 0;
   height: 100%;
@@ -784,7 +530,6 @@ html {
   opacity: 0;
 }
 .ksvuefp-section, .ksvuefp-section__overlay {
-  height: 0%;
   width: 0%;
   top: 0;
   left: 0;
